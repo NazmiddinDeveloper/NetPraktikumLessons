c42d40ad9b8b6c5adcef420403d2454b7ea1b5cf
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-07-14 00:22:36 +0500

    a

---
+++ C#1-bolim/lesson19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("char data type");
+
+char smallA = 'a';
+char capitalA = 'A';
+char umpersaint = '&';
+
+Console.WriteLine($"{smallA} ->ASCII-> {(short)smallA}");
+Console.WriteLine($"{capitalA} ->ASCII-> {(short)capitalA}");
+Console.WriteLine($"{umpersaint} ->ASCII-> {(short)umpersaint}");
+
+string smallb = "b";
+Console.WriteLine(smallb);
+
+
+
+
+Console.ReadKey();
